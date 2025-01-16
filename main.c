0b10fbfc553ef9f887f726b691f89b91fdbda4b5
202304727 <r202304727ci@jindai.jp>
2025-01-16 08:28:29 +0000

    new for loop

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 
 int main(void){
   int n = 10;
-  for(int i = 0; i < n; i++)  printf("%d\n", i);
+  for(int i = 0; i < n; i++)  printf("%d ", i);
+  printf("\n");
   printf("hello world!\n");
   return 0;
 }
